c78ec3cd0906320d41901a2ce6b0d7e0e3569f79
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-02-27 08:00:32 +1100

    Add initial Kitura files

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "ibis"
+    name: "ibis",
+    dependencies: [
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6)
+    ]
 )
