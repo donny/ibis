2a673a4cfc99db2b494519e485e8d5f24d8eb94e
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-02-28 09:28:31 +1100

    Implement initial Redis code

--- Package.swift
+++ Package.swift
@@ -4,6 +4,8 @@ let package = Package(
     name: "ibis",
     dependencies: [
       .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
-      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6)
+      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
+      .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 6),
+      .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15, minor: 0)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,12 +1,60 @@
 import Kitura
 import HeliumLogger
 import Foundation
+import SwiftRedis
+import SwiftyJSON
 
 // Initialize HeliumLogger
 HeliumLogger.use()
 
 // Create a new router
 let router = Router()
+
+// Redis
+let redis = Redis()
+
+let redisInfo = ProcessInfo.processInfo.environment["VCAP_SERVICES"] ?? "{}"
+let redisInfoJson = JSON.parse(string: redisInfo)
+let redisCreds = redisInfoJson["rediscloud"][0]["credentials"]
+
+let rHost = redisCreds["hostname"].string ?? ""
+let rPort = Int32(redisCreds["port"].string ?? "0") ?? 0
+let rPassword = redisCreds["password"].string ?? ""
+
+
+print("Redis \(rHost) \(rPort) \(rPassword)")
+
+
+redis.connect(host: rHost, port: rPort) { (redisError: NSError?) in
+    if let error = redisError {
+        print("________")
+        print(error)
+    }
+    else {
+      print("________")
+        print("Connected to Redis")
+        // set a key
+
+        redis.auth(rPassword) { (redisError: NSError?) in
+
+          redis.set("Redis", value: "on Swift") { (result: Bool, redisError: NSError?) in
+              if let error = redisError {
+                  print(error)
+              }
+              // get the same key
+              redis.get("Redis") { (string: RedisString?, redisError: NSError?) in
+                  if let error = redisError {
+                      print(error)
+                  }
+                  else if let string = string?.asString {
+                      print("Redis \(string)")
+                  }
+              }
+          }
+
+        }
+    }
+}
 
 // Handle HTTP GET requests to /
 router.get("/") {
